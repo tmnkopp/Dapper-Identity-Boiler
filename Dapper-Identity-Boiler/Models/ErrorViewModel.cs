f433954bab5f20350fdef87b20fa5dd052b6db71
Tim Kopp <tim.kopp@cyber-balance.com>
2020-12-28 13:18:40 -0500

    Add project files.

---
+++ Dapper-Identity-Boiler/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dapper_Identity_Boiler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
